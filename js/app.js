3fd8a7d9c8cf8e367fd0bd4522340be9271048bd
JuanFRomero <JuanF.RomeroG@gmail.com>
2020-03-26 00:24:28 -0500

    Comparadores Js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//TIPOS DE VARIABLE
+
 let letras = 'Juan';
 let booleano;
 const numero = 23;
@@ -19,3 +21,17 @@ console.log (typeof  simbolo )
 console.log (arreglo )
 console.log (objeto )
 console.log (fecha )
+
+
+// OPERADORES DE COMPARACION
+
+const numero1 = 20;
+const numero2 = 50;
+const numero3 = '20';
+
+console.log(numero1 > numero2)
+console.log(numero2 > numero1)
+console.log(numero1 > numero3)
+
+console.log(numero1 == numero3) // compara valor sin ver tipo de dato
+console.log(numero1 === numero3)// compara valor viendo el tipo de dato
