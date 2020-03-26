b32cf685af7675ed0c6a2ff7a68971513527a0fd
JuanFRomero <JuanF.RomeroG@gmail.com>
2020-03-26 00:16:55 -0500

    Tipos de variable Js

---
+++ js/app.js
@@ -0,0 +1,21 @@
+let letras = 'Juan';
+let booleano;
+const numero = 23;
+booleano = false;
+
+let simbolo = Symbol('simbolo');
+let arreglo = [0,1,2]
+let objeto ={
+    nombre: 'Juan',
+    apellido: 'Romero'
+}
+
+let fecha = new Date();
+
+console.log (typeof letras  )
+console.log (typeof  numero )
+console.log (typeof booleano )
+console.log (typeof  simbolo )
+console.log (arreglo )
+console.log (objeto )
+console.log (fecha )
